5b46c6160b3caeb2048812ad02a90ffff3fd68f7
Martin937 <Martin937@yandex.ru>
2025-05-08 13:03:42 +0300

    step 1

--- js/app.js
+++
@@ -1,7 +0,0 @@
-export function openPage(url) {
-	window.location.href = url;
-}
-
-export function confirmAction(message) {
-	return confirm(message);
-}
--- js/variables.js
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_IMAGE = "assets/img/default.jpg";
-export const DB_NAME = "RecipeDB";
-export const DB_VERSION = 1;
-export const STORE_NAME = "recipes";
